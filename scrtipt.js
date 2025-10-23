26ae8421f894bce8414fc374c0a072acb0a926e1
Quack923 <vanya.pich@gmail.com>
2025-10-24 01:03:19 +1000

    hw2 24.10.25

---
+++ scrtipt.js
@@ -0,0 +1,50 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+let firstYear = 2007;
+alert(firstYear);
+
+let autor = "BrendanEich";
+alert(autor);
+
+
+let n1 = 10;
+let n2 = 2;
+alert (sum = n1+n2);
+alert (difference = n1-n2);
+alert(product=n1*n2);
+alert(quotient=n1/n2);
+
+let result = 2 ** 5;
+alert(result);
+
+let aa = 9;
+let b = 2;
+alert(quotient=aa/b);
+
+let num = 1;
+num += 5;
+num-= 3;
+num*= 7;
+num/= 3;
+num+= 1;
+num-= 1;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+
+alert(age);
+
+
+const user = {
+   name: 'Vanya',
+   age: '23',
+   isAdmin:'false',
+}
+
+
+const userName = prompt("Как вас зоувт?");
+alert(`Привет, ${userName}!`);
+
