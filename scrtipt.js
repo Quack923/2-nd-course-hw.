80f502b7b716bad7b02458f4397714fd1b2f352a
Quack923 <vanya.pich@gmail.com>
2025-10-25 22:38:38 +1000

    Hw2 try2 25.10

--- scrtipt.js
+++ scrtipt.js
@@ -1,31 +1,49 @@
-let password =(12345);
-let UserPassword = (prompt ('Введите пароль:'));
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
 
-if (UserPassword===password){console.log("Пароль введен верно");}
-    
- else {console.log("Пароль введен неверно");}
-    
-let c =(7);
-if (c>0&&c<10) { console.log("Верно");
-    
-} else {console.log("Неверно");
-    
+let firstYear = 2007;
+alert(firstYear);
+
+let autor = "BrendanEich";
+alert(autor);
+
+
+let n1 = 10;
+let n2 = 2;
+alert (sum = n1+n2);
+alert (difference = n1-n2);
+alert(product=n1*n2);
+alert(quotient=n1/n2);
+
+let result = 2 ** 5;
+alert(result);
+
+let aa = 9;
+let b = 2;
+alert(quotient=aa/b);
+
+let num = 1;
+num += 5;
+num-= 3;
+num*= 7;
+num/= 3;
+num+= 1;
+num-= 1;
+alert(num);
+
+let age = prompt("Сколько вам лет?");
+
+alert(age);
+
+
+const user = {
+   name: 'Vanya',
+   age: '23',
+   isAdmin:'false',
 }
 
 
-let d =(70);
-let e = (120);
-
-if (d>100||e>100) {console.log("Верно");
-    
-} else {console.log("Неверно");
-    
-}
-
-let a = '2';
-let b = '3';
-console.log(parseInt("a"));
-console.log(parseInt("b"));
-
-
-alert(a + b);
+const userName = prompt("Как вас зоувт?");
+alert(`Привет, ${userName}!`);
