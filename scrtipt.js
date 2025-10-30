11963a96ffa60f76b63701f3e21c05b533480aae
Quack923 <vanya.pich@gmail.com>
2025-10-31 02:50:16 +1000

    hw3 30.10

--- scrtipt.js
+++ scrtipt.js
@@ -1,58 +1,75 @@
-let a = 10;
-alert(a);
-a = 20;
-alert(a);
+let password = (12345);
+let UserPassword = (prompt ('Введите пароль:'));
 
-let firstYear = 2007;
-alert(firstYear);
-
-let autor = "BrendanEich";
-alert(autor);
-
-
-let n1 = 10;
-let n2 = 2;
- let result1 = ( n1+n2);
- alert(result1);
- let result2 = ( n1-n2);
- alert(result2);
- let result3 = (n1*n2);
- alert(result3);
- let result4 = (n1/n2);
- alert(result4);
-
- 
-
-let result = 2 ** 5;
-alert(result);
-
-let aa = 9;
-let b = 2;
-let remainder = (aa%b);
-alert(remainder);
-
-
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num += 1;
-num -= 1;
-alert(num);
-
-let age = prompt("Сколько вам лет?");
-
-alert(age);
-
-
-const user = {
-   name: 'Vanya',
-   age: 23,
-   isAdmin:false,
+if (UserPassword===password){alert("Пароль введен верно");}
+    
+ else {alert("Пароль введен неверно");}
+    
+let c =(7);
+if (c>0&&c<10) { console.log("Верно");
+    
+} else {console.log("Неверно");
+    
 }
 
 
-const userName = prompt("Как вас зоувт?");
-alert(`Привет, ${userName}!`);
+let d =(70);
+let e = (120);
 
+if (d>100||e>100) {console.log("Верно");
+    
+} else {console.log("Неверно");
+    
+}
+
+let a = '2';
+let b = '3';
+Number(a);
+Number(b);
+alert(a+b);
+
+
+let day = prompt("Введите номер или название месяца и узнайте какой это сезон");
+
+switch (month) {
+   case 'январь'||'первый':
+      console.log('Зима');
+      break;
+   case 'февраль'||'Второй':
+      console.log('Зима');
+      break;
+   case 'март'||'Третий':
+      console.log('Весна');
+      break;
+   case 'апрель'||'Четвертый':
+      console.log('Весна');
+      break;
+   case 'май'||'Пятый':
+      console.log('Весна');
+      break;
+   case 'июнь'||'Шестой':
+      console.log('Лето');
+      break;
+   case 'июль'||'Седьмой':
+      console.log('Лето');
+      break;
+   case 'август'||'Восьмой':
+      console.log('Лето');
+      break;
+   case 'сентябрь'||'Девятый':
+      console.log('Осень');
+      break;
+   case 'октябрь'||'Десятый':
+      console.log('Осень');
+      break;
+   case 'ноябрь'||'Одинадцатый':
+      console.log('Осень');
+      break;
+   case 'декабрь'||'Двенадцатый':
+      console.log('Зима');
+      break;
+   
+   default:
+      console.log('ввели что-то не то');
+      break;
+}
